a5ffefc8d158f578ae57d6014f11fd977b95a3dd
spacestudiomx <spacestudiomx@gmail.com>
2019-08-10 00:45:06 -0500

    Se agrega Capas de trabajo al proyecto entyty framework

---
+++ Retail Point of Sale/Datalayer/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Datalayer.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MyDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(MyDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
